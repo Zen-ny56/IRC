2e4b46a19ae8415a75f595e2dc4edfa04bc74cd2
Zeni <147077236+Zen-ny56@users.noreply.github.com>
2024-12-08 12:19:56 +0000

    Socket creation, binding and poll waiting have been written but not tested

---
+++ srcs/ClientManager.cpp
@@ -0,0 +1,2 @@
+#include "ClientManager.hpp"
+
--- srcs/ClientManager.hpp
+++ srcs/ClientManager.hpp
@@ -6,6 +6,8 @@ class ClientManager
         std::vector<Client> clients;
 
     public:
+        Client();
+        ~Client();
         void addClient(int fd);
         void removeClient(int fd);
         void handleClients(const std::vector<int> &activeSockets, CommandProcessor &processor);
