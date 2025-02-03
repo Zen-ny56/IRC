061a5d737a8a8a92cbf4e9e76e7e73ab382eb6de
Oryema Allan <34004636+AllanRye9@users.noreply.github.com>
2025-02-03 21:58:02 +0000

    Updated

--- script.sh
+++ script.sh
@@ -1,6 +1,8 @@
 if git status | grep -w "modified" > /dev/null; then
     if ls -la | grep -w ".vscode" > /dev/null; then
         rm -f .vscode
+        echo .vscode found and removed successfully;
     fi
     git add .; git commit -m "Updated"; git push;
+    echo File successfully git push;
 fi
